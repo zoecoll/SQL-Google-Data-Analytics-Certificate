705b364b943a5a85c4fcd0e721d4aa5ac0269447
zoecoll <107445862+zoecoll@users.noreply.github.com>
2023-07-26 18:41:39 -0500

    Update Analyze Data to Answer Questions.sql

--- Analyze Data to Answer Questions.sql
+++ Analyze Data to Answer Questions.sql
@@ -1,2 +1,9 @@
 SELECT
-  U
+  usertype,
+  CONCAT(start_station_name, "to", end_station_name) AS route,
+	COUNT(*) as num_trips,
+	ROUND(AVG(cast(tripduration as int64)/60),2) AS duration
+FROM
+		`bigquery-public-data.new_york.citibike_trips`
+GROUP BY
+		start_station_name, end_station_name, usertype
