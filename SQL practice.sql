1b0477e4ecd2e3eee637bef70d4da311f6b921d1
zoecollins <107445862+zoecollins@users.noreply.github.com>
2024-03-30 13:02:25 -0500

    Update SQL practice.sql

--- SQL practice.sql
+++ SQL practice.sql
@@ -1,5 +1,4 @@
 /* 1 */
-
 SELECT tweet_count_per_user AS tweet_bucket,
 COUNT (user_id) AS users_num
 FROM (
@@ -12,7 +11,6 @@ GROUP BY user_id) AS total_tweets
 GROUP BY tweet_count_per_user;
 
 /* 2 */
-
 SELECT candidate_id
 FROM candidates
 WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
@@ -22,8 +20,12 @@ HAVING COUNT (skill) = 3
 ORDER BY candidate_id;
 
 /* 3 */
-
 SELECT pages.page_id
 FROM pages
 LEFT OUTER JOIN page_likes ON pages.page_id = page_likes.page_id
 WHERE page_likes.page_id IS NULL;
+
+/* 4 */
+SELECT part, assembly_step
+FROM parts_assembly
+WHERE finish_date is NULL;
