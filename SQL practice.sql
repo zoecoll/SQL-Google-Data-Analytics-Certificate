cb0d015401e05ce34747299808e9e79ab1101a62
zoecollins <107445862+zoecollins@users.noreply.github.com>
2024-03-30 16:39:48 -0500

    Update SQL practice.sql

--- SQL practice.sql
+++ SQL practice.sql
@@ -45,3 +45,25 @@ FROM posts
 WHERE DATE_PART('year', post_date::DATE) = 2021
 GROUP BY user_id
 HAVING COUNT (post_id)>1;
+
+/* 7 - Write a query to identify the top 2 Power Users who sent the highest number of messages on Microsoft Teams in August 2022. 
+Display the IDs of these 2 users along with the total number of messages they sent. 
+Output the results in descending order based on the count of the messages.*/
+SELECT 
+sender_id,
+COUNT(message_id) AS message_count
+FROM messages
+WHERE sent_date BETWEEN '8/1/2022' AND '8/31/2022'
+GROUP BY sender_id
+ORDER BY message_count DESC
+LIMIT 2;
+
+## or ##
+  
+SELECT 
+  sender_id,
+  COUNT(message_id) AS count_messages
+FROM messages
+WHERE EXTRACT(MONTH FROM sent_date) = '8'
+  AND EXTRACT(YEAR FROM sent_date) = '2022'
+GROUP BY sender_id;
