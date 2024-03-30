e6b5825ffa18bea02ca450cb5d2dc10b330d4970
zoecollins <107445862+zoecollins@users.noreply.github.com>
2024-03-30 14:43:20 -0500

    Update SQL practice.sql

--- SQL practice.sql
+++ SQL practice.sql
@@ -35,3 +35,13 @@ SELECT
 COUNT(*) FILTER (WHERE device_type = 'laptop') AS laptop_views,
 COUNT(*) FILTER (WHERE device_type IN ('tablet', 'phone')) AS mobile_deviews
 FROM viewership;
+
+/* 6 - for each user who posted at least twice in 2021, 
+write a query to find the number of days between each user’s first post of the year and last post of the year in the year 2021.*/
+SELECT 
+  user_id,
+  MAX(post_date::DATE) - MIN(post_date::DATE) AS days_between
+FROM posts
+WHERE DATE_PART('year', post_date::DATE) = 2021
+GROUP BY user_id
+HAVING COUNT (post_id)>1;
