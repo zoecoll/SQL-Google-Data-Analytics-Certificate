d8c4a87307be0ac29edf370fe0cd32acd84f4cef
zoecollins <107445862+zoecollins@users.noreply.github.com>
2024-03-31 11:12:31 -0500

    Update SQL practice.sql

--- SQL practice.sql
+++ SQL practice.sql
@@ -97,3 +97,17 @@ SELECT *
 FROM cte_name;
 
 /***************/
+
+/*Write a query to retrieve the top three cities that have the highest number of completed trade orders listed in descending order. 
+  Output the city name and the corresponding number of completed trade orders.*/
+
+SELECT 
+  users.city, 
+  COUNT(trades.order_id) AS total_orders 
+FROM trades 
+INNER JOIN users 
+  ON trades.user_id = users.user_id 
+WHERE trades.status = 'Completed' 
+GROUP BY users.city 
+ORDER BY total_orders DESC
+LIMIT 3;
