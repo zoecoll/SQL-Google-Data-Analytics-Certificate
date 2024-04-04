be21d4b0bd0739286819ecd0f31d81a86796283e
zoecollins <107445862+zoecollins@users.noreply.github.com>
2024-04-03 22:22:27 -0500

    Update SQL practice DataLemur.sql

--- SQL practice DataLemur.sql
+++ SQL practice DataLemur.sql
@@ -150,3 +150,12 @@ GROUP BY product
 ORDER BY mth;
 
 ## Correct ##
+SELECT 
+  EXTRACT(MONTH FROM submit_date) AS mth,
+  product_id,
+  ROUND(AVG(stars), 2) AS avg_stars
+FROM reviews
+GROUP BY 
+  EXTRACT(MONTH FROM submit_date), 
+  product_id
+ORDER BY mth, product_id;
